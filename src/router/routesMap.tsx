5b0e06583c62df7d830c7457663bfd7c991a6c33
RajovicMirko <rajovicmirko@gmail.com>
2023-03-29 09:50:54 +0200

    Cleanup

--- src/router/routesMap.tsx
+++ src/router/routesMap.tsx
@@ -3,15 +3,15 @@ import PostPage from "src/pages/Post";
 import UserPage from "src/pages/User";
 import UsersPage from "src/pages/Users";
 
-type RouteCustomProps = RouteProps & {
-  path: RoutePath;
-};
-
 export enum RoutePath {
   users = "/",
   user = "/user",
   post = "/user/post",
 }
+
+type RouteCustomProps = RouteProps & {
+  path: RoutePath;
+};
 
 const routesMap: RouteCustomProps[] = [
   { path: RoutePath.users, element: <UsersPage /> },
