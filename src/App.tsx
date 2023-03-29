fc4bc43dea78e44f8b1baa0518230772737c87e6
RajovicMirko <rajovicmirko@gmail.com>
2023-03-29 09:37:16 +0200

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import RouterView from "./router";
-
-import Layout from "./components/Layout";
-
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import Layout from "src/components/Layout";
 import { PageLoading } from "src/components/Loading/PageLoading";
 import { AuthProvider } from "src/context/AuthContext";
 import { LoadingProvider } from "src/context/LoadingContext";
 import { NotificationProvider } from "src/context/Notification";
 import ErrorBoundary from "src/pages/ErrorBoundary";
+import RouterView from "src/router";
 import MaterialUiProvider from "src/style";
 
 const queryClient = new QueryClient({});
